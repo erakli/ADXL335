e866d32a937a629c04506f58b4e107daf422529d
Egor Panfilov <erakli00@yandex.com>
2018-02-21 23:33:44 +0300

    Initial update of base code

--- ADXL335.cpp
+++ ADXL335.cpp
@@ -6,138 +6,175 @@
 
 #include "ADXL335.h"
 
-ADXL335::ADXL335(int pin_x, int pin_y, int pin_z, float aref)
-{
-  _aref = aref;
-  _pin_x = pin_x;
-  _pin_y = pin_y;
-  _pin_z = pin_z;
-  _mvG = aref / 10.0;
-  _bias = aref / 2.0;
-  _rad2deg = 180.0 / M_PI;
+const float rad2deg = 180.0 / M_PI;
+
+bool FuzzyIsNull(float f) {
+    return (abs(f) <= 0.00001f);
 }
 
+
+ADXL335::ADXL335(int pin_x, int pin_y, int pin_z, float aref, 
+                 float zero_x, float zero_y, float zero_z)
+    : _pin_x(pin_x)
+    , _pin_y(pin_y)
+    , _pin_z(pin_z)
+    , _aref(aref)
+    , _mvG(aref / 10.0) // datasheet says 1G typically corresponds to 330 mV voltage difference
+    , _deadzone(0.0)
+{
+    float default_bias = aref / 2.0;
+
+    if ( !FuzzyIsNull(zero_x) )
+        _bias[0] = zero_x;
+    else
+        _bias[0] = default_bias;
+
+    if ( !FuzzyIsNull(zero_y) )
+        _bias[1] = zero_y;
+    else
+        _bias[1] = default_bias;
+
+    if ( !FuzzyIsNull(zero_z) )
+        _bias[2] = zero_z;
+    else
+        _bias[2] = default_bias;
+}
+
+
+//begin public methods
+
+void ADXL335::setThreshold(float deadzone)
+{
+    _deadzone = deadzone;
+}
+
+
+//gets whether the device is in free fall
+bool ADXL335::getFreefall()
+{
+    //if all three vectors read zero then return true, otherwise; false.
+    return (_xg == 0.0 && _yg == 0.0 && _zg == 0.0);
+}
+
+
+float ADXL335::getX()
+{
+    return _xg;
+}
+
+
+float ADXL335::getY()
+{
+    return _yg;
+}
+
+
+float ADXL335::getZ()
+{
+    return _zg;
+}
+
+
+float ADXL335::getRho()
+{
+    return _getRho(_xg, _yg, _zg);
+}
+
+
+float ADXL335::getPhi()
+{
+    return _getPhi(_xg, _yg, _zg);
+}
+
+
+float ADXL335::getTheta()
+{
+    return _getTheta(_xg, _yg, _zg);
+}
+
+
+void ADXL335::update()
+{
+    _xg = getGravity(analogRead(_pin_x), 0);
+    _yg = getGravity(analogRead(_pin_y), 1);
+    _zg = getGravity(analogRead(_pin_z), 2);
+}
+
+//end public methods
+
+
+
 //begin private methods
+
 float ADXL335::geta2d(float gx, float gy)
 {
-  float a;
-  
-  a = gx * gx;
-  a = fma(gy,gy,a);
-  
-  return sqrt(a);
+    float a = gx * gx;
+    a = fma(gy, gy, a);
+
+    return sqrt(a);
 }
+
 
 //gets the magnitude of the 3d vector
 //the formula is a^2 = x^2 + y^2 + z^2
 float ADXL335::geta3d(float gx, float gy, float gz)
 {
-  float a;
-  
-  //use floating point multiply-add cpu func
-  //sometimes we get better precision
-  a = gx * gx;
-  a = fma(gy,gy,a);
-  a = fma(gz,gz,a);
-  
-  return sqrt(a);
+    //use floating point multiply-add cpu func
+    //sometimes we get better precision
+    float a = gx * gx;
+    a = fma(gy, gy, a);
+    a = fma(gz, gz, a);
+
+    return sqrt(a);
 }
 
-void ADXL335::processDeadzone(float* gv)
+
+void ADXL335::processDeadzone(float *gv)
 {
-  if (*gv < _deadzone && *gv > -_deadzone)
-  {
-    *gv = 0.0;
-  }
+    if (abs(*gv) < _deadzone)
+    {
+        *gv = 0.0;
+    }
 }
 
-float ADXL335::getGravity(int reading)
-{  
-  float voltage = reading * _aref;
-  voltage /= 1024.0;
 
-  //minus the zero g bias 
-  //then divide by mv/g
-  //which when Vs = 3.3V, V/g = 0.330
-  float gv = (voltage - _bias) / _mvG;
-  
-  //minus the null zone
-  processDeadzone(&gv);
-  
-  return gv;
+float ADXL335::getGravity(int reading, int axis)
+{
+    float voltage = reading * _aref;
+    voltage /= ADC_AMPLITUDE;
+
+    //minus the zero g bias
+    //then divide by mv/g
+    //which when Vs = 3.3V, V/g = 0.330
+    float gv = (voltage - _bias[axis]) / _mvG;
+
+    //minus the null zone
+    processDeadzone(&gv);
+
+    return gv;
 }
+
 
 float ADXL335::_getRho(float ax, float ay, float az)
 {
-  return geta3d(_xg,_yg,_zg);
+    return geta3d(_xg, _yg, _zg);
 }
+
 
 float ADXL335::_getPhi(float ax, float ay, float az)
 {
-  return atan2(ay, ax) * _rad2deg;  
+    return atan2(ay, ax) * rad2deg;
 }
+
 
 float ADXL335::_getTheta(float ax, float ay, float az)
 {
-  float rho = _getRho(ax, ay, az);
-  
-  if (rho == 0.0)
-    return NAN;
-  else
-    return acos(az / rho) * _rad2deg;
+    float rho = _getRho(ax, ay, az);
+
+    if (FuzzyIsNull(rho))
+        return NAN;
+    else
+        return acos(az / rho) * rad2deg;
 }
 
 //end private methods
-
-
-//begin public methods
-void ADXL335::setThreshold(float deadzone)
-{
-  _deadzone = deadzone;
-}
-
-//gets whether the device is in free fall
-boolean ADXL335::getFreefall()
-{
-  //if all three vectors read zero then return true, otherwise; false.
-  return _xg == 0.0 && _yg == 0.0 && _zg == 0.0;
-}
-
-float ADXL335::getX()
-{
-  return _xg;
-}
-
-float ADXL335::getY()
-{
-  return _yg;
-}
-
-float ADXL335::getZ()
-{
-  return _zg;
-}
-
-float ADXL335::getRho()
-{
-  return _getRho(_xg,_yg,_zg);
-}
-
-float ADXL335::getPhi()
-{
-  return _getPhi(_xg,_yg,_zg);
-}
-
-float ADXL335::getTheta()
-{
-  return _getTheta(_xg,_yg,_zg);
-}
-
-void ADXL335::update()
-{
-  _xg = getGravity(analogRead(_pin_x));
-  _yg = getGravity(analogRead(_pin_y));
-  _zg = getGravity(analogRead(_pin_z));
-}
-
-//end public methods
--- ADXL335.h
+++ ADXL335.h
@@ -7,43 +7,52 @@
 #ifndef ADXL335_h
 #define ADXL335_h
 
-#if (ARDUINO >= 100)
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
+#include "Arduino.h"
+
+#define ADC_AMPLITUDE 1024.0 //amplitude of the 10bit-ADC of Arduino is 1024LSB
 
 class ADXL335
 {
   public:
-    ADXL335(int pin_x, int pin_y, int pin_z, float aref);
+    ADXL335(int pin_x, int pin_y, int pin_z, float aref, 
+            float zero_x = 0.0, float zero_y = 0.0, float zero_z = 0.0);
+
     void setThreshold(float deadzone);
-    boolean getFreefall();
+    bool getFreefall();
+
     float getX();
     float getY();
     float getZ();
+
     float getRho();
     float getPhi();
     float getTheta();
+
     void update();
+
   private:
     float geta2d(float gx, float gy);
     float geta3d(float gx, float gy, float gz);
-    void processDeadzone(float* gv);
-    float getGravity(int reading);
+
+    void processDeadzone(float *gv);
+    float getGravity(int reading, int axis);
+
     float _getRho(float ax, float ay, float az);
     float _getPhi(float ax, float ay, float az);
     float _getTheta(float ax, float ay, float az);
+
     int _pin_x;
     int _pin_y;
     int _pin_z;
-    float _bias;
+
+    float _aref;
+    float _mvG;
+    float _bias[3] = {0};
+
+    float _deadzone;
+
     float _xg;
     float _yg;
     float _zg;
-    float _aref;
-    float _mvG;
-    float _deadzone;
-    float _rad2deg;
 };
 #endif
